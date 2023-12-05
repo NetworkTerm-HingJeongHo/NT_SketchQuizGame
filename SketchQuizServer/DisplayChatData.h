853303eede731a4288d7037da5d954f143c76af3
Sung JI Yoon <66246253+SungJiYoon@users.noreply.github.com>
2023-12-05 15:29:23 +0900

    채팅 데이터 출력 함수 작성(txt 파일 불러오는건 아직)

---
+++ SketchQuizServer/DisplayChatData.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// 채팅 데이터 view 핸들
+static HWND g_hChatListView;
+
+void InitializeChatListView(HWND hWnd);
+void AddChatMessageToListView(_TCHAR* id, _TCHAR* message);
+void DisplayChatList();
